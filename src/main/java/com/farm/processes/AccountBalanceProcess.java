0eb6e0759649a04bf482377324486e3863856404
PolexRufous <polex.rufous@gmail.com>
2017-02-25 20:46:54 +0200

    Work with account balance process

--- src/main/java/com/farm/processes/AccountBalanceProcess.java
+++ src/main/java/com/farm/processes/AccountBalanceProcess.java
@@ -1,11 +1,14 @@
 package com.farm.processes;
 
+import com.farm.database.accounts.Account;
 import com.farm.database.accounts.AccountBalance;
 import com.farm.database.accounts.AccountBalanceRepository;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
+
+import static java.util.Objects.isNull;
 
 /**
  * Farm project. 2017
@@ -18,7 +21,10 @@ public class AccountBalanceProcess
   @Resource
   private AccountBalanceRepository accountBalanceRepository;
 
-  public AccountBalance findOrCreateByAccountNumber(){
-
+  public AccountBalance findOrCreateByAccount(Account account){
+    AccountBalance accountBalance = accountBalanceRepository.findByAccount(account);
+    if (isNull(accountBalance)){
+      
+    }
   }
 }
