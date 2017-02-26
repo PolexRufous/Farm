c04b82e978b1e0d9efee51e9888bc5e123856c1f
Karen Asmarian <dreadful.nemo@gmail.com>
2017-02-26 18:28:39 -0500

    add groovy

---
+++ src/test/groovy/com.farm.database.accounts/GlobalDatabaseSpec.groovy
@@ -0,0 +1,11 @@
+package com.farm.database.accounts
+
+import spock.lang.Specification
+
+class GlobalDatabaseSpec extends Specification {
+
+   def "should be deleted"() {
+        expect:
+        1 == 1
+    }
+}
