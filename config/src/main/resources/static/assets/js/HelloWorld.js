2ed4b159aff4e170e7bfed22bde645bc976f7c0e
Karen Asmarian <karen.asmarian@johnlewis.co.uk>
2017-03-14 15:36:59 +0000

    merged all to master

--- config/src/main/resources/static/assets/js/HelloWorld.js
+++
@@ -1,6 +0,0 @@
-$(document).load(function(event) {
-    ReactDOM.render(
-        React.createElement("h1", null, "Welcome to our cosy farm. Get yourself a rake and START WORKING!"),
-        document.getElementById('welcome-test')
-    );
-});
